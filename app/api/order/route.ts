48e2fc3d87bfb574da8b7f7e5611ef834abb0a9d
Smit Ghelani <ghelanismit4@gmail.com>
2024-04-05 09:26:41 +0530

    Order API done

--- app/api/order/route.ts
+++ app/api/order/route.ts
@@ -1,9 +1,9 @@
 import { NextRequest } from "next/server";
 import {OrderModel} from "@/app/models/orderModel"
 const POST = async (req: NextRequest) => { 
-    const {name, email, shippingAddress} = {
-        
-    }
+    const {name, email, shippingAddress, orderDetails, userDetails} = await req.json()
+
+    
 }
 
 export {POST};
