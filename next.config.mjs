618361cae33e8653a0440604124c766482a7345b
Smit Ghelani <ghelanismit4@gmail.com>
2024-10-02 21:35:04 +0530

    Cors change and orgin allow changes added in next-config

--- next.config.mjs
+++ next.config.mjs
@@ -36,7 +36,7 @@ const nextConfig = {
         return [
             {
                 // matching all API routes
-                source: "https://ecommerce.smitghelani.xyz/api/:path*",
+                source: "/api/:path*",
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
                     { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
