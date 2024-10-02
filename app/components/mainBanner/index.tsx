7d4b7cceaafe41f09ea4aa7f519cb6b461da9ae5
Smit Ghelani <ghelanismit4@gmail.com>
2024-10-02 22:44:13 +0530

    Cors change and orgin allow changes added in next-config

--- app/components/mainBanner/index.tsx
+++ app/components/mainBanner/index.tsx
@@ -8,7 +8,7 @@ const MainBanner = () => {
                 <div className="lg:grid lg:grid-cols-2 lg:gap-8 lg:items-center">
                     <div>
                         <h2 className="text-3xl font-extrabold text-white">Seasonal Sale</h2>
-                        <p className="mt-3 max-w-3xl text-lg leading-6 text-indigo-200">Check out our latest collection on sale this season. Don't miss out on our best prices!</p>
+                        <p className="mt-3 max-w-3xl text-lg leading-6 text-indigo-200">Check out our latest collection on sale this season. Don`t miss out on our best prices!</p>
                         <div className="mt-8 sm:flex">
                             <div className="rounded-md shadow">
                                 <Link href="/products" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-indigo-50 md:py-4 md:text-lg md:px-10"> Shop Now </Link>
